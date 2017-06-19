81b737e938e611179001cec716661b9ac1dc47b5
Julia Wrobel <jw3134@cumc.columbia.edu>
2017-06-19 12:01:51 -0400

    initializing R package

---
+++ R/cats.R
@@ -0,0 +1,14 @@
+#' A Cat Function
+#'
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+cat_function <- function(love=TRUE){
+  if(love==TRUE){
+    print("I love cats!")
+  }
+  else {
+    print("I am not a cool person.")
+  }
+}
